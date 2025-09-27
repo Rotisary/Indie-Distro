92eaacafa60afd2ad01fea47e562d125c526cd0c
Oladotun Kolapo <oladotunkolapo@gmail.com>
2025-09-27 14:16:32 +0100

    celery configuration

--- config/__init__.py
+++ config/__init__.py
@@ -3,6 +3,8 @@ import sys
 from pathlib import Path
 import environ
 
+from .celery import app as celery_app
+
 
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # This allows easy placement of apps within the interior
@@ -15,4 +17,4 @@ env_dir = os.path.join(BASE_DIR, ".env")
 if os.path.exists(env_dir):
     environ.Env.read_env(env_dir, overwrite=True)
 
-# __all__ = ("celery_app",)
+__all__ = ("celery_app",)
