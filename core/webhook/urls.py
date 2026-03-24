40a41e68cde93fe399075cf1f5ee5d59ffe98d22
Oladotun Kolapo <oladotunkolapo@gmail.com>
2026-03-24 14:07:57 +0100

    remove webhook crud system(retain only exposed flutterwave post endpoint)

--- core/webhook/urls.py
+++ core/webhook/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from .views import WebhookEndpointListCreate, WebhookEndpointUpdate, FlutterwaveWebhook
+from .views import FlutterwaveWebhook
 
 urlpatterns = [
-    path("endpoints/", WebhookEndpointListCreate.as_view(), name="webhook-endpoints"),
-    path("endpoints/<int:pk>", WebhookEndpointUpdate.as_view(), name="webhook-endpoints-update"),
     path("flutterwave/", FlutterwaveWebhook.as_view(), name="flutterwave-webhook"),
 ]
--- core/webhook/views.py
+++ core/webhook/views.py
@@ -1,69 +1,13 @@
-import hmac
-import hashlib
-
 from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import views, status, response
 from rest_framework.parsers import JSONParser
-from rest_framework.permissions import IsAuthenticated
 
 from loguru import logger
-from drf_spectacular.utils import extend_schema
-
-from .models import WebhookEndpoint
-from .serializers import WebhookEndpointSerializer
-from core.utils.helpers.authenticators import ServerAuthentication
+ 
 from core.utils.helpers.payment.handlers import PaymentHandlers
 
-
-@extend_schema(tags=["webhooks"])
-class WebhookEndpointListCreate(views.APIView):
-    http_method_names = ["post", "get"]
-    authentication_classes = [ServerAuthentication, ]
-
-    @extend_schema(
-        description="endpoint to add a new webhook url",
-        request=WebhookEndpointSerializer.WebhookListCreate, 
-        responses={201: WebhookEndpointSerializer.WebhookListCreate}
-    )
-    def post(self, request):
-        serializer = WebhookEndpointSerializer.WebhookListCreate(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        logger.success(f"webhook endpoint added for user {instance.owner.id} with id: {instance.id}")
-        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-    @extend_schema(
-        description="get list of webhook endpoints(filter by user or global webhook endpoints)",
-        request=None,
-        responses={200: WebhookEndpointSerializer.WebhookListCreate(many=True)}
-    )
-    def get(self, request):
-        qs = WebhookEndpoint.objects.filter(owner=request.user) | WebhookEndpoint.objects.filter(owner__isnull=True)
-        serializer = WebhookEndpointSerializer(qs, many=True)
-        return response.Response(serializer.data)
-    
-
-@extend_schema(tags=["webhooks"])
-class WebhookEndpointUpdate(views.APIView):
-    http_method_names = ["patch", "get"]
-    authentication_classes = [ServerAuthentication, ]
-
-    @extend_schema(
-        description="endpoint to update a webhook url",
-        request=WebhookEndpointSerializer.WebhookUpdate, 
-        responses={201: WebhookEndpointSerializer.WebhookListCreate}
-    )
-    def patch(self, request, pk):
-        instance = WebhookEndpoint.objects.get(id=pk)
-        serializer = WebhookEndpointSerializer.WebhookUpdate(instance, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        instance = serializer.save()
-        logger.success(f"webhook endpoint updated for user {instance.owner.id} with id: {instance.id}")
-        serializer = WebhookEndpointSerializer.WebhookListCreate(instance=instance)
-        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class FlutterwaveWebhook(views.APIView):
